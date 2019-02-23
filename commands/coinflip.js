a63eebdad16a86e9d05d5a57ec009972891ecc26
Tyler Marenger <tjmareng@mtu.edu>
2019-02-22 23:31:25 -0500

    Importing Images -- Coinflip prints img [Heads/Tails]

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,20 +1,43 @@
+const Discord = require("discord.js");
+const collector = new Discord.Client();
+
 exports.run = (client, message, args) => {
+
+    /*
+    message.channel.send('', {
+        files: [
+            "./Images/coinflip.gif" // GIF to send
+        ]
+    });
+    */
+
+    // TO DO
+    //message.channel.send('Heads or tails?');
 
     // Get Random (0 or 1)
     var coin = getRandomInt(1);
 
+    // TO DO -- Ask Heads or tails
+
     // If 0 its Heads
     if (coin == 0) {
-        message.channel.send("Pinging...").then(function (m) {
-            m.edit(`:coin: **HEADS** - Took: ${m.createdTimestamp - message.createdTimestamp}ms`)
-        })
-    
+        // Send Text Above 
+        message.channel.send('**HEADS**', {
+            files: [
+                "./Images/heads.jpg" // Image to send
+            ]
+        });
+
         // Otherwise Tails
     } else {
-        message.channel.send("Pinging...").then(function (m) {
-            m.edit(` :coin: **TAILS** - Took: ${m.createdTimestamp - message.createdTimestamp}ms`)
-        })
+        // Send Text Above 
+        message.channel.send('**TAILS**', {
+            files: [
+                "./Images/tails.jpg" // Image to send
+            ]
+        });
     }
+
 }
 
 // Returns Random Number 0 - Max
