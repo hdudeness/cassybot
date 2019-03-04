99cc0b20646bcce5bf2ce3cb39eeac80a7563923
Tyler Marenger <tjmareng@mtu.edu>
2019-03-04 15:44:02 -0500

    Delete join_channel.js -- Not Used

--- commands/music/join_channel.js
+++
@@ -1,7 +0,0 @@
-const commando = require('discord.js-commando');
-
-class JoinChannelCommand extends commando.Command{
-    async run(message, args){
-        message.member.voiceChannel.join();
-    }
-}
