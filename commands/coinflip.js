583f1c38f0398001d3c17c3e2c126f1a637c0739
Tyler Marenger <tjmareng@mtu.edu>
2019-03-19 13:56:11 -0400

    Fixed coinflip loss bug

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -17,7 +17,7 @@ exports.run = (client, message, args) => {
     function getUserInput() {
         // Get bet amount
         if (!betConfirm) {
-            const betCollector = new Discord.MessageCollector(message.channel, m => m.author.id == message.author.id, {time: 10000});
+            const betCollector = new Discord.MessageCollector(message.channel, m => m.author.id == message.author.id, { time: 10000 });
             message.channel.send(`Welcome to coinflip! Enter your bet amount...`);
             console.log(betCollector);
             betCollector.on('collect', message => {
@@ -39,8 +39,8 @@ exports.run = (client, message, args) => {
             })
         }
         else {
-        // Get heads or tails
-            const choiceCollector = new Discord.MessageCollector(message.channel, m => m.author.id == message.author.id, {time: 10000});
+            // Get heads or tails
+            const choiceCollector = new Discord.MessageCollector(message.channel, m => m.author.id == message.author.id, { time: 10000 });
             message.channel.send(`Now enter heads or tails...`);
             console.log(choiceCollector);
             choiceCollector.on('collect', message => {
@@ -72,25 +72,25 @@ exports.run = (client, message, args) => {
         // Heads win
         if (coinDecider > .5 && userChoice == "h") {
             win = true;
-            db.exec("UPDATE currency SET credits = credits " + ((win)?"+":"-") +" "+ bet +" WHERE id = " + message.author.id +";");
+            db.exec("UPDATE currency SET credits = credits " + ((win) ? "+" : "-") + " " + bet + " WHERE id = " + message.author.id + ";");
             message.reply(`you won **` + bet + ` credits!** You now have ${currency.credits + bet} credits.`);
         }
         // Tails win
         else if (coinDecider < .5 && userChoice == "t") {
             win = true;
-            db.exec("UPDATE currency SET credits = credits " + ((win)?"+":"-") +" "+ bet +" WHERE id = " + message.author.id +";");
+            db.exec("UPDATE currency SET credits = credits " + ((win) ? "+" : "-") + " " + bet + " WHERE id = " + message.author.id + ";");
             message.reply(`you won **` + bet + ` credits!** You now have ${currency.credits + bet} credits.`);
         }
         // Loss
         else {
             win = false;
-            db.exec("UPDATE currency SET credits = credits " + ((win)?"+":"-") +" "+ bet +" WHERE id = " + message.author.id +";");
-            message.reply(`you lost **` + bet + ` credits!** You now have ${currency.credits + bet} credits.`);
+            db.exec("UPDATE currency SET credits = credits " + ((win) ? "+" : "-") + " " + bet + " WHERE id = " + message.author.id + ";");
+            message.reply(`you lost **` + bet + ` credits!** You now have ${currency.credits - bet} credits.`);
         }
     }
 
     getUserInput();
-    
+
     // If CassyBot -> Test
     if (message.author == client.user) {
         buff.coin = true;
