969dc70eabdac664a03121f63d7d9761f6499ad5
Tyler Marenger <tjmareng@mtu.edu>
2019-03-20 11:40:17 -0400

    Refine Test Suite -- Prints to txt file

--- commands/testsuite.js
+++ commands/testsuite.js
@@ -1,26 +1,32 @@
 const buff = require('./testbuffer.js');
+const fs = require('fs');
 
-exports.run = async(client, message, args) => {
-    
+exports.run = async (client, message, args) => {
+
     // TEST HELP
     message.channel.send('-------------------------------------------------------')
     message.channel.send('!help')
-    const msg = await message.channel.awaitMessages(msg => message.content.includes("help"), {time: 5000}); 
+    const msg = await message.channel.awaitMessages(msg => message.content.includes("help"), { time: 5000 });
 
-    if(!buff.help){
+    if (!buff.help) {
         console.log(buff.help)
         message.reply('**ERROR**')
         return;
     } else {
         message.channel.send('**Passed**');
+        fs.appendFile('testsuite.txt', `Test Help - Passed\n`, function (err) {
+            if (err) {
+                console.log('Unable to write to file');
+            }
+        });
     }
-    
+
     // TEST COINFLIP
     message.channel.send('-------------------------------------------------------')
     message.channel.send('!coinflip')
-    const msgCoin = await message.channel.awaitMessages(msgCoin => message.content.includes("coinflip"), {time: 5000});
+    const msgCoin = await message.channel.awaitMessages(msgCoin => message.content.includes("coinflip"), { time: 5000 });
 
-    if(!buff.coin){
+    if (!buff.coin) {
         console.log(buff.coin)
         message.reply('**ERROR**')
         return;
@@ -42,13 +48,13 @@ exports.run = async(client, message, args) => {
         message.channel.send('**Passed**');
     }
     */
-   
+
     // TEST PING
     message.channel.send('-------------------------------------------------------')
     message.channel.send('!ping')
-    const msgPing = await message.channel.awaitMessages(msgPing => message.content.includes("ping"), {time: 5000});
+    const msgPing = await message.channel.awaitMessages(msgPing => message.content.includes("ping"), { time: 5000 });
 
-    if(!buff.ping){
+    if (!buff.ping) {
         console.log(buff.ping)
         message.reply('**ERROR**')
         return;
@@ -59,9 +65,9 @@ exports.run = async(client, message, args) => {
     // TEST TABLEFLIP
     message.channel.send('-------------------------------------------------------')
     message.channel.send('(╯°□°）╯︵ ┻━┻')
-    const msgTable= await message.channel.awaitMessages(msgTable => message.content.includes("(╯°□°）╯︵ ┻━┻"), {time: 5000});
+    const msgTable = await message.channel.awaitMessages(msgTable => message.content.includes("(╯°□°）╯︵ ┻━┻"), { time: 5000 });
 
-    if(!buff.table){
+    if (!buff.table) {
         console.log(buff.table)
         message.reply('**ERROR**')
         return;
@@ -86,9 +92,9 @@ exports.run = async(client, message, args) => {
     // TEST LEADERBOARDS
     message.channel.send('-------------------------------------------------------')
     message.channel.send('!leaderboards')
-    const msgLeader= await message.channel.awaitMessages(msgLeader => message.content.includes("leaderboards"), {time: 5000});
-    
-    if(!buff.leader){
+    const msgLeader = await message.channel.awaitMessages(msgLeader => message.content.includes("leaderboards"), { time: 5000 });
+
+    if (!buff.leader) {
         console.log(buff.leader)
         message.reply('**ERROR**')
         return;
@@ -99,9 +105,9 @@ exports.run = async(client, message, args) => {
     // BUGGY TEST -- INTENTIONAL
     message.channel.send('-------------------------------------------------------')
     message.channel.send('!buggycommand')
-    const msgBug = await message.channel.awaitMessages(msgBug => msgBug.content.includes("buggycommand"), {time: 5000});
+    const msgBug = await message.channel.awaitMessages(msgBug => msgBug.content.includes("buggycommand"), { time: 5000 });
 
-    if(!buff.bug){
+    if (!buff.bug) {
         console.log(buff.bug)
         message.reply('**ERROR**')
         return;
@@ -110,4 +116,4 @@ exports.run = async(client, message, args) => {
     }
 }
 
-config: {}
+config: { }
