3da51c5afac26fa77d285ea2cf1e7d44164785f0
Tyler Marenger <tjmareng@mtu.edu>
2019-03-31 11:53:26 -0400

    Added Random Colors to Slot Embed Output

--- commands/games/slot.js
+++ commands/games/slot.js
@@ -1,5 +1,6 @@
 exports.run = (client, message, args) => {
     const Discord = require("discord.js");
+    var colors = [0x115DA8, 0xDC143C, 0xFF4500, 0xFFD700, 0x00FF00];
 
     let slots = ["🍎", "🍌", "🍊", "🍐", "🍒"];
     let result1 = Math.floor((Math.random() * slots.length));
@@ -8,20 +9,20 @@ exports.run = (client, message, args) => {
     let name = message.author.displayName;
     let icon = message.author.displayAvatarURL;
 
-    if(slots[result1] == slots[result2] && slots[result3]){
+    if (slots[result1] == slots[result2] && slots[result3]) {
         let wEmbed = new Discord.RichEmbed()
             .setFooter('You won!', icon)
             .setTitle(':slot_machine: Slots :slot_machine:')
             .addField('Result:', slots[result1] + slots[result2] + slots[result3], true)
-            .setColor(0xF4E842)
+            .setColor(colors[Math.floor(Math.random() * colors.length)])
         message.channel.send(wEmbed)
     }
-    else{
+    else {
         let lEmbed = new Discord.RichEmbed()
             .setFooter('You lost!', icon)
             .setTitle(':slot_machine: Slots :slot_machine:')
             .addField('Result:', slots[result1] + slots[result2] + slots[result3], true)
-            .setColor(0xF4E842)
+            .setColor(colors[Math.floor(Math.random() * colors.length)])
         message.channel.send(lEmbed)
     }
 }
